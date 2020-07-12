e7e00dbe7ad1b746ddfc5c151b593d6185d8ea42
Asfiolitha Wilmarani <asfiolithaw@yahoo.co.id>
2020-07-12 23:12:44 +0700

    added placeholder card in kanal informasi

--- src/pages/kanalInfo.js
+++ src/pages/kanalInfo.js
@@ -1,22 +1,58 @@
 import React from 'react'
 
 import Container from 'react-bootstrap/Container'
+import CardColumns from 'react-bootstrap/CardColumns'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import Landing from '../components/landing'
 
+import kanal from '../data/kanalInfo.json'
 import Styles from '../styles/kanalInfo'
 
 export default function kanalInfo() {
     const title = "Kanal Informasi"
     const desc = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis cum ab voluptates ullam excepturi doloribus nihil voluptate quos consectetur delectus?"
- 
+    const data = kanal.info
+
     return (
         <Styles>
             <Navbar/>
             <Landing page="kanalInfo" color="#F03F91" title={title} desc={desc} />
             <Container>
-                <h1>Kanal Informasi</h1>
+                <CardColumns>
+                    {data.map( item => (
+                        <Card>
+                            <Card.Body> 
+                                <div className="cardHeader">
+                                    <img alt={item.title} src={ require(`../assets/logo/${item.logo}`) } />
+                                    <h2>{item.title}</h2>
+                                </div>           
+                                <Row>
+                                    <Col xs={7}><p>{item.desc}</p></Col> 
+                                    <Col xs={5}>
+                                        {item.contacts[0].narahubung.map( person => (
+                                            <span>
+                                                {person.nama}<br />
+                                                <i class="fas fa-phone-square-alt"></i> {person.hp}<br />
+                                                <img alt="line" src={ require(`../assets/logo/line.png`) } /> {person.line}<br/>
+                                            </span>
+                                        ))}<br/>
+                                        {item.contacts[1].socmed.telp && (<span><i class="fas fa-phone-volume"></i> {item.contacts[1].socmed.telp}<br/></span>)}
+                                        {item.contacts[1].socmed.fax && (<span><i class="fas fa-fax"></i>(fax) {item.contacts[1].socmed.fax}<br/></span>)}
+                                        {item.contacts[1].socmed.email && (<span><i class="fas fa-envelope-square"></i> {item.contacts[1].socmed.email}<br/></span>)}
+                                        {item.contacts[1].socmed.twitter && (<span><i class="fab fa-twitter"></i>{item.contacts[1].socmed.twitter}<br/></span>)}
+                                        {item.contacts[1].socmed.ig && (<span><i class="fab fa-instagram"></i> {item.contacts[1].socmed.ig}<br/></span>)}
+                                        {item.contacts[1].socmed.fb && (<span><i class="fab fa-facebook-square"></i> {item.contacts[1].socmed.fb}<br/></span>)}
+                                        {item.contacts[1].socmed.web && (<span><i class="fas fa-globe"></i> {item.contacts[1].socmed.web}<br/></span>)}
+                                    </Col>   
+                                </Row>  
+                            </Card.Body>                            
+                        </Card>
+                    ))}
+                </CardColumns>
             </Container>
             <div className="gap"></div>
             <Footer />
--- src/styles/kanalInfo.js
+++ src/styles/kanalInfo.js
@@ -7,6 +7,48 @@ const Styles = styled.div`
     .navbar-light{
         background: #FF62D1;
     }
+
+    h2 {
+        font-weight: bold;
+    }
+
+    span img {
+        height: 18px;
+    }
+
+    .cardHeader {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 1rem;
+    }
+
+    .cardheader img {
+        width: 125px;
+    }
+
+    .card-columns {
+        column-count: 1;
+    }
+
+    .card {
+        border-radius: 30px;
+        border: none;
+        box-shadow: 3px 3px 50px rgba(24, 24, 24, 0.15);
+        padding: 1rem;
+    }
+
+    .card:hover{
+        background: rgba( 255, 255, 255, .50);
+        h2 { 
+            opacity: .75;
+        }
+    }
     
+    @media (min-width: 768px) {
+        .card-columns {
+            column-count: 2;
+        }
+    }
 `
 export default Styles
