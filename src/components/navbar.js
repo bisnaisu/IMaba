6e7f53026a6144ae42ad7493b4bef2114c83784c
Asfiolitha Wilmarani <asfiolithaw@yahoo.co.id>
2020-08-04 16:09:08 +0700

    changed dropdown links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -28,11 +28,11 @@ const navbar = (props) => {
                                     <Dropdown as="div">                                                                            
                                         <Dropdown.Toggle as="div"><NavLink to="/akademik" activeStyle={selected}>Akademik</NavLink></Dropdown.Toggle>
                                         <Dropdown.Menu>
-                                            <Dropdown.Item href="/akademik#belajarApa">Belajar Apa</Dropdown.Item>
-                                            <Dropdown.Item href="/akademik#ilkomSI">Ilkom vs. SI</Dropdown.Item>
-                                            <Dropdown.Item href="/akademik#matkul">Mata Kuliah</Dropdown.Item>
-                                            <Dropdown.Item href="/akademik#kurikulum">Kurikulum</Dropdown.Item>
-                                            <Dropdown.Item href="/akademik#scele">Scele</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/akademik#belajarApa">Belajar Apa</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/akademik#ilkomSI">Ilkom vs. SI</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/akademik#matkul">Mata Kuliah</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/akademik#kurikulum">Kurikulum</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/akademik#scele">Scele</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </Nav.Item>
@@ -42,13 +42,13 @@ const navbar = (props) => {
                                             <NavLink to="/kampus" activeStyle={selected}>Info Kampus</NavLink>
                                         </Dropdown.Toggle>
                                         <Dropdown.Menu>
-                                            <Dropdown.Item href="/kampus#fakultas">Fakultas</Dropdown.Item>
-                                            <Dropdown.Item href="/kampus#petaUI">Peta UI &amp; Rute Bikun</Dropdown.Item>
-                                            <Dropdown.Item href="/kampus#landmark">Landmark UI</Dropdown.Item>
-                                            <Dropdown.Item href="/kampus#transportasi">Transportasi</Dropdown.Item>
-                                            <Dropdown.Item href="/kampus#infoKehidupan">Info Kehidupan</Dropdown.Item>
-                                            <Dropdown.Item href="/kampus#fitur">Fitur Sivitas UI</Dropdown.Item>
-                                            <Dropdown.Item href="/kampus#beasiswa">Beasiswa</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#fakultas">Fakultas</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#petaUI">Peta UI &amp; Rute Bikun</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#landmark">Landmark UI</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#transportasi">Transportasi</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#infoKehidupan">Info Kehidupan</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#fitur">Fitur Sivitas UI</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/kampus#beasiswa">Beasiswa</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </Nav.Item>
@@ -58,11 +58,11 @@ const navbar = (props) => {
                                             <NavLink to="/fasilkom" activeStyle={selected}>Info Fasilkom</NavLink>
                                         </Dropdown.Toggle>
                                         <Dropdown.Menu>
-                                            <Dropdown.Item href="/fasilkom#budaya">Budaya Fasilkom</Dropdown.Item>
-                                            <Dropdown.Item href="/fasilkom#pacilLife">Living in Fasilkom</Dropdown.Item>
-                                            <Dropdown.Item href="/fasilkom#ngoding101">Ngoding101</Dropdown.Item>
-                                            <Dropdown.Item href="/fasilkom#denah">Denah Fasilkom</Dropdown.Item>
-                                            <Dropdown.Item href="/fasilkom#yelyel">Mars dan Yel-Yel</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/fasilkom#budaya">Budaya Fasilkom</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/fasilkom#pacilLife">Living in Fasilkom</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/fasilkom#ngoding101">Ngoding101</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/fasilkom#denah">Denah Fasilkom</Dropdown.Item>
+                                            <Dropdown.Item href="/imaba/fasilkom#yelyel">Mars dan Yel-Yel</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </Nav.Item>   
